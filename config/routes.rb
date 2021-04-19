7e0e91f3ea94b8b8d0a9817e4dd375094f21ace0
Cynthia <cinthya3259@gmail.com>
2021-04-19 15:59:36 -0600

    add update views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
-Rails.application.routes.draw do
-  resources :carts, only: %i[ show update ]
-  devise_for :admins
+Rails.application.routes.draw do  
+  resource :carts, only: %i[show update]
   devise_for :users
+  devise_for :admins
   root 'home#index'
-
+  
   authenticate :admin do 
     resources :products
     resources :categories
